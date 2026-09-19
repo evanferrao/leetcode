62f22467f0cba38b95868a0b67edaba0fc27a365
Evan Ferrao <evanferrao@gmail.com>
2026-09-19 18:58:24 +0530

    Time: 411 ms (94.20%) | Memory: 0B (100.00%) - LeetSync

--- 619-biggest-single-number/biggest-single-number.sql
+++ 619-biggest-single-number/biggest-single-number.sql
@@ -1,9 +1,8 @@
+# Write your MySQL query statement below
 SELECT MAX(num) AS num
 FROM (
     SELECT num
     FROM MyNumbers
     GROUP BY num
     HAVING COUNT(num) = 1
-    ORDER BY num DESC
-    LIMIT 1
 ) AS t;
