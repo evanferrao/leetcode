75fcd070d6f74aa6bf337f19c78dd627715d1de3
Evan Ferrao <evanferrao@gmail.com>
2026-09-20 11:09:30 +0530

    Time: 660 ms (78.12%) | Memory: 0B (100.00%) - LeetSync

--- 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
+++ 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
@@ -2,7 +2,7 @@
 SELECT
     E1.employee_id,
     E1.name, 
-    COUNT(E1.employee_id) as reports_count,
+    COUNT(E2.employee_id) as reports_count, # we can also use COUNT(E1.employee_id) as they will have the same number after the join. E2 just makes more sense as we're counting the number of employees reporting. E1.employee_id will count the manager for every employee present.
     ROUND(AVG(E2.age)) as average_age
 FROM Employees E1
 JOIN Employees E2
