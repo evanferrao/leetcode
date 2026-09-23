06da2bef79501cf34975f00daaf5d06378eb987d
Evan Ferrao <evanferrao@gmail.com>
2026-09-23 22:20:41 +0530

    Time: 823 ms (31.83%) | Memory: 0B (100.00%) - LeetSync

--- 2087-confirmation-rate/confirmation-rate.sql
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -1,7 +1,7 @@
 # Write your MySQL query statement below
 SELECT
     S.user_id,
-    ROUND(COALESCE((SUM(C.action = 'confirmed')/COUNT(S.user_id)),0),2) AS confirmation_rate
+    ROUND(COALESCE((COUNT(CASE WHEN C.action = 'confirmed' THEN 1 END)/COUNT(S.user_id)),0),2) AS confirmation_rate
 FROM Signups S
 LEFT JOIN Confirmations C
 ON (S.user_id = C.user_id)
